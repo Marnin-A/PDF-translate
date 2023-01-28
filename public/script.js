54d4cb20ecc5a60a21b7ed159f68308cbbfb160a
Marnin Audu <msmaudu@gmail.com>
2023-01-28 18:25:04 +0100

    Upadted styling and html to display selected file

--- public/script.js
+++ public/script.js
@@ -1,6 +1,12 @@
 const inpFile = document.getElementById("inpFile");
 const btnUpload = document.getElementById("btnUpload");
 const resultText = document.getElementById("resultText");
+const actualBtn = document.getElementById("inpFile");
+const fileChosen = document.getElementById("file-chosen");
+
+actualBtn.addEventListener("change", function () {
+  fileChosen.textContent = this.files[0].name;
+});
 
 btnUpload.addEventListener("click", () => {
   const formData = new FormData();
